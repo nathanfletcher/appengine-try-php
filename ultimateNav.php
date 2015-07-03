7bd197d2eb661ddf3ed3a8ff0a9a44420b442685
fletchern <fletchern@fletchern-macbookpro.roam.corp.google.com>
2015-07-03 16:54:52 +0000

    Initial commit

---
+++ ultimateNav.php
@@ -0,0 +1,25 @@
+<div style="margin-bottom:50px">
+<nav class="navbar navbar-fixed-top navbar-default" >
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Abby's Projects</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="/bbs">BBS</a></li>
+              <li><a href="/taivani">Taivani</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="../navbar-fixed-top/">About Me</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+      </div>
